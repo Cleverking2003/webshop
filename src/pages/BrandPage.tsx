30e5583534d63c39d280e590ce96206ec91aaa01
Egor Ananyin <ananinegor@gmail.com>
2024-05-18 11:46:11 +0300

    Image upload works

---
+++ src/pages/BrandPage.tsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import { get_brands, get_categories } from "../api/products";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { Brand, Category } from "../Interfaces";
+
+const BrandPage = () => {
+
+    const {
+        data,
+        isLoading,
+        error,
+        isFetchingNextPage,
+        fetchNextPage,
+        hasNextPage,
+    } = useInfiniteQuery(["items"], get_brands, {
+        getNextPageParam: (page: any) => page.next,
+    });
+    if (error instanceof Error) return <>{toast.error(error.message)}</>;
+
+    return (
+        <>
+            {
+                data?.pages.map((page: any) => (
+                    <div className="flex justify-center">
+                        <div className="p-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-16">
+
+                            {page.results.map((brand: Brand) => (
+                                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                    <Link to={`/brand/${brand.id}`}>
+                                        <img
+                                            className="rounded-t-lg"
+                                            src="/static/logo.png"
+                                            alt=""
+                                        />
+                                    </Link>
+                                    <div className="p-5 ">
+                                        <Link to={`/brand/${brand.id}`}>
+                                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                {brand.name}
+                                            </h5>
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))}
+                            
+                        </div>
+                    </div>
+                ))
+            }
+        </>
+    );
+};
+export default BrandPage;
---
+++ src/pages/SearchByBrand.tsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { brand_api, cate_api } from "../api/products";
+import Loader from "../components/Loader";
+import { toast } from "react-hot-toast";
+import { Product } from "../Interfaces";
+import Rating from "../components/Rating";
+import { useCartStore } from "../store/cart";
+import ProductCard from "../components/ProductCard";
+
+const SearchByBrand = () => {
+
+    const { brand } = useParams()
+    const addToCart = useCartStore(state => state.addToCart)
+
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['items', brand],
+        queryFn: () => brand_api(brand || ''),
+    })
+
+    if (isError) return toast.error("Error!")
+    if (isLoading) return <Loader />
+
+    console.log(data)
+
+    return (
+        <div className="flex justify-center">
+            <div className="p-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-16">
+                
+                {data && data.results.map((product: Product) => (
+                    <ProductCard
+                        key={product.id}
+                        product={product}
+                    />
+                ))}
+
+            </div>
+        </div>
+    )
+    
+}
+export default SearchByBrand;
